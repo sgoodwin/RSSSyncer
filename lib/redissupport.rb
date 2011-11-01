99a9e7e1e2a75c7269a0f4e19804852297e45cdd
Samuel Goodwin <goodwinlabs@gmail.com>
2011-10-31 21:33:08 -0400

    Silly mistake.

--- lib/redissupport.rb
+++ lib/redissupport.rb
@@ -5,6 +5,7 @@ module RedisSupport
 	def redis
 		redis_URL = URI::parse(ENV['REDISTOGO_URL'] || "redis://0.0.0.0:6379")
 		@@redis ||= Redis.new(:host => redis_URL.host, :port => redis_URL.port)
-		@@redis.auth(redis_URL.password);
+		@@redis.auth(redis_URL.password)
+		return @@redis
 	end
 end
