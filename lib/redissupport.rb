179f463a4d69f68864ec47f58af146a0f95b1ed6
Samuel Goodwin <goodwinlabs@gmail.com>
2011-10-31 21:32:25 -0400

    Can't forget the password

--- lib/redissupport.rb
+++ lib/redissupport.rb
@@ -5,5 +5,6 @@ module RedisSupport
 	def redis
 		redis_URL = URI::parse(ENV['REDISTOGO_URL'] || "redis://0.0.0.0:6379")
 		@@redis ||= Redis.new(:host => redis_URL.host, :port => redis_URL.port)
+		@@redis.auth(redis_URL.password);
 	end
 end
